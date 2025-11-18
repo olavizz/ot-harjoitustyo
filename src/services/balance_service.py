8c54e035d8f9f625e708838a672db288b9c1733c
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 19:51:22 +0200

    Add tests and coverage conf

--- src/services/balance_service.py
+++ src/services/balance_service.py
@@ -1,16 +1,16 @@
 class BalanceService:
 
-    def __init__(self):
-        self._balance = "0"
+    def __init__(self, balance=0):
+        self._balance = str(balance)
     
     def increase_balance(self, increment):
         value = self._balance
-        self._balance = int(value) + int(increment)
+        self._balance = str(int(value) + int(increment))
         return self._balance
     
     def decrease_balance(self, decrement):
         value = self._balance
-        self._balance = int(value) - int(decrement)
+        self._balance = str(int(value) - int(decrement))
         return self._balance
     
     def _get_balance(self):
