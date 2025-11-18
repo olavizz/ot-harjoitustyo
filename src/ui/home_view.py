bb8ab84bbaef3019f5b2f1caf9f1686353beed32
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 13:58:15 +0200

    modify expense ui view

--- src/ui/home_view.py
+++ src/ui/home_view.py
@@ -33,8 +33,12 @@ class HomeView:
         self._balance_entry = ttk.Entry(master=self._frame)
 
         self._new_expense = ttk.Label(master=self._frame, text="Add a new expense", font=(30))
-        self._expense_entry = ttk.Entry(master=self._frame)
+        self._expense_name = ttk.Label(master=self._frame, text="product or servide,", font=(20))
+        self._expense_price = ttk.Label(master=self._frame, text="price €", font=(20))
+        self._expense_ps_entry = ttk.Entry(master=self._frame)
+        self._expense_price_entry = ttk.Entry(master=self._frame)
         self._expense_entry_button = ttk.Button(master=self._frame, text="Enter", command=self._add_expense)
+        self._new_expense_amount = ttk.Entry(master=self._frame)
 
         self._balance_amount = ttk.Label(master=self._frame, textvariable=self._balance_var)
         self._expenses_amount = ttk.Label(master=self._frame, textvariable=self._expenses_var)
@@ -56,9 +60,13 @@ class HomeView:
         self._balance_entry.grid(row=2, column=0)
         self._increase_button.grid(row=2, column=1)
         self._decrease_button.grid(row=2, column=2)
+
         self._new_expense.grid(row=3, column=0, sticky=constants.W, padx=10, pady=10)
-        self._expense_entry.grid(row=4, column=0, sticky=constants.W, padx=10 ,pady=10)
-        self._expense_entry_button.grid(row=4, column=1, sticky=constants.W, padx=10)
+        self._expense_name.grid(row=4, column=0, sticky=constants.W, padx=10, pady=10)
+        self._expense_ps_entry.grid(row=4, column=1, sticky=constants.W, padx=10 ,pady=10)
+        self._expense_price.grid(row=5, column=0, sticky=constants.W, padx=10)
+        self._expense_price_entry.grid(row=5, column=1, sticky=constants.W, padx=10)
+        self._expense_entry_button.grid(row=6, column=1, sticky=constants.W, padx=10)
     
     def _increase_balance(self):
         value = self._balance_var.get()
@@ -73,8 +81,9 @@ class HomeView:
         self._balance_var.set(str(new_balance))
     
     def _add_expense(self):
-        #expense = self._expense_entry
-        pass
+        new_expense = self._expense_ps_entry.get()
+        new_expense_price = self._expense_price_entry.get()
+        self.expenses_list.append((new_expense, new_expense_price))
     
     def _show_expenses(self):
         pass
