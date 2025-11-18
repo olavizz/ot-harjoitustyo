8d2762b032e49a6be506a721fbb6bd0b0c5c1767
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 13:32:59 +0200

    Add new expenses view

--- src/ui/home_view.py
+++ src/ui/home_view.py
@@ -5,6 +5,7 @@ class HomeView:
         self._root = root
         self._balance_var = None
         self._expenses_var = None
+        self.expenses_list = []
         self._frame = None 
         self._service = HomeService()
 
@@ -31,6 +32,10 @@ class HomeView:
         self._expenses = ttk.Label(master=self._frame, text="expenses", font=(20))
         self._balance_entry = ttk.Entry(master=self._frame)
 
+        self._new_expense = ttk.Label(master=self._frame, text="Add a new expense", font=(30))
+        self._expense_entry = ttk.Entry(master=self._frame)
+        self._expense_entry_button = ttk.Button(master=self._frame, text="Enter", command=self._add_expense)
+
         self._balance_amount = ttk.Label(master=self._frame, textvariable=self._balance_var)
         self._expenses_amount = ttk.Label(master=self._frame, textvariable=self._expenses_var)
         self._increase_button = ttk.Button(master=self._frame, text="increase", command=self._increase_balance)
@@ -51,6 +56,9 @@ class HomeView:
         self._balance_entry.grid(row=2, column=0)
         self._increase_button.grid(row=2, column=1)
         self._decrease_button.grid(row=2, column=2)
+        self._new_expense.grid(row=3, column=0, sticky=constants.W, padx=10, pady=10)
+        self._expense_entry.grid(row=4, column=0, sticky=constants.W, padx=10 ,pady=10)
+        self._expense_entry_button.grid(row=4, column=1, sticky=constants.W, padx=10)
     
     def _increase_balance(self):
         value = self._balance_var.get()
@@ -63,6 +71,13 @@ class HomeView:
         decrement = self._balance_entry.get()
         new_balance = self._service.decrease_balance(value, decrement)
         self._balance_var.set(str(new_balance))
+    
+    def _add_expense(self):
+        #expense = self._expense_entry
+        pass
+    
+    def _show_expenses(self):
+        pass
 
 
 class HomeService:
