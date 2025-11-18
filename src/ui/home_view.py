0ba45bf36fb091755f4f6702c1b023323619c242
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 13:11:48 +0200

    refactoring homeview

--- src/ui/home_view.py
+++ src/ui/home_view.py
@@ -6,6 +6,7 @@ class HomeView:
         self._balance_var = None
         self._expenses_var = None
         self._frame = None 
+        self._service = HomeService()
 
         self._initialize()
 
@@ -15,40 +16,61 @@ class HomeView:
     def destroy(self):
         self._frame.destroy()
     
-    def _increase_balance(self):
-        value = self._balance_var.get()
-        increment = self._balance_entry.get()
-        self._balance_var.set(str(int(value) + int(increment)))
-    
-    def _decrease_balance(self):
-        value = self._balance_var.get()
-        decrement = self._balance_entry.get()
-        self._balance_var.set(str(int(value) - int(decrement)))
-    
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
 
+        self._init_variables()
+        self._create_widgets()
+        self._layout_widgets()
+
+        
+    def _create_widgets(self):
+        self._frame = ttk.Frame(master=self._root)
+
+        self._balance = ttk.Label(master=self._frame, text="balance", font=(20))
+        self._expenses = ttk.Label(master=self._frame, text="expenses", font=(20))
+        self._balance_entry = ttk.Entry(master=self._frame)
+
+        self._balance_amount = ttk.Label(master=self._frame, textvariable=self._balance_var)
+        self._expenses_amount = ttk.Label(master=self._frame, textvariable=self._expenses_var)
+        self._increase_button = ttk.Button(master=self._frame, text="increase", command=self._increase_balance)
+        self._decrease_button = ttk.Button(master=self._frame, text="decrease", command=self._decrease_balance)
+    
+
+    def _init_variables(self):
         self._balance_var = StringVar()
         self._balance_var.set("0")
         self._expenses_var = StringVar()
         self._expenses_var.set("0")
-
-        balance = ttk.Label(master=self._frame, text="balance", font=(20))
-        expenses = ttk.Label(master=self._frame, text="expenses", font=(20))
-        self._balance_entry = ttk.Entry(master=self._frame)
-
-        balance_amount = ttk.Label(master=self._frame, textvariable=self._balance_var)
-        expenses_amount = ttk.Label(master=self._frame, textvariable=self._expenses_var)
-        increase_button = ttk.Button(master=self._frame, text="increase", command=self._increase_balance)
-        decrease_button = ttk.Button(master=self._frame, text="decrease", command=self._decrease_balance)
-
-        balance.grid(row=0, column=0, sticky=constants.W, padx=10)
-        balance_amount.grid(row=0, column=1, sticky=constants.W, padx=10)
-        expenses.grid(row=1, column=0, sticky=constants.W, padx=10)
-        expenses_amount.grid(row=1, column=1, sticky=constants.W, padx=10)
+    
+    def _layout_widgets(self):
+        self._balance.grid(row=0, column=0, sticky=constants.W, padx=10)
+        self._balance_amount.grid(row=0, column=1, sticky=constants.W, padx=10)
+        self._expenses.grid(row=1, column=0, sticky=constants.W, padx=10)
+        self._expenses_amount.grid(row=1, column=1, sticky=constants.W, padx=10)
         self._balance_entry.grid(row=2, column=0)
-        increase_button.grid(row=2, column=1)
-        decrease_button.grid(row=2, column=2)
+        self._increase_button.grid(row=2, column=1)
+        self._decrease_button.grid(row=2, column=2)
+    
+    def _increase_balance(self):
+        value = self._balance_var.get()
+        increment = self._balance_entry.get()
+        new_balance = self._service.increase_balance(value, increment)
+        self._balance_var.set(str(new_balance))
+    
+    def _decrease_balance(self):
+        value = self._balance_var.get()
+        decrement = self._balance_entry.get()
+        new_balance = self._service.decrease_balance(value, decrement)
+        self._balance_var.set(str(new_balance))
 
-    def dashboard(self):
-        pass
+
+class HomeService:
+    
+    def increase_balance(self, value, increment):
+        return int(value) + int(increment)
+    
+    def decrease_balance(self, value, decrement):
+        return int(value) - int(decrement)
+    
+    
