38379e0a9b90dbe376061ca897fe6ea09fe8456f
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 02:06:26 +0200

    Add increase balance feature

--- src/ui/home_view.py
+++ src/ui/home_view.py
@@ -15,6 +15,10 @@ class HomeView:
     def destroy(self):
         self._frame.destroy()
     
+    def _increase_balance(self):
+        value = self._balance_entry.get()
+        self._balance_var.set(value)
+    
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
 
@@ -25,14 +29,18 @@ class HomeView:
 
         balance = ttk.Label(master=self._frame, text="balance", font=(20))
         expenses = ttk.Label(master=self._frame, text="expenses", font=(20))
+        self._balance_entry = ttk.Entry(master=self._frame)
 
         balance_amount = ttk.Label(master=self._frame, textvariable=self._balance_var)
         expenses_amount = ttk.Label(master=self._frame, textvariable=self._expenses_var)
+        increase_button = ttk.Button(master=self._frame, text="increase", command=self._increase_balance)
 
         balance.grid(row=0, column=0, sticky=constants.W, padx=10)
         balance_amount.grid(row=0, column=1, sticky=constants.W, padx=10)
         expenses.grid(row=1, column=0, sticky=constants.W, padx=10)
         expenses_amount.grid(row=1, column=1, sticky=constants.W, padx=10)
+        self._balance_entry.grid(row=2, column=0)
+        increase_button.grid(row=2, column=1)
 
     def dashboard(self):
         pass
