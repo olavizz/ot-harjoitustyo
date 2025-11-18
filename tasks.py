2ea7b9444014cf9faee19632d5defc55b76ae5cd
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 20:02:18 +0200

    Add invoke

---
+++ tasks.py
@@ -0,0 +1,20 @@
+
+from invoke import task
+from subprocess import call
+from sys import platform
+
+@task
+def start(ctx):
+    ctx.run("python3 src/app.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
