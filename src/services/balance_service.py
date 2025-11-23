5646b2c8a43f3f3f809f83aefe9934487d83670b
Olavi Suojala <o.suojala@gmail.com>
2025-11-23 19:00:11 +0200

    Add total earnings label

--- src/services/balance_service.py
+++ src/services/balance_service.py
@@ -2,11 +2,13 @@ class BalanceService:
 
     def __init__(self, balance=0):
         self._balance = str(balance)
+        self._total_earnings = str(balance)
 
     def increase_balance(self, increment):
         value = self._balance
         self._balance = str(int(value) + int(increment))
-        return self._balance
+        self._total_earnings = (str(int(self._total_earnings) + int(increment)))
+        return [self._balance, self._total_earnings]
 
     def decrease_balance(self, decrement):
         value = self._balance
@@ -15,5 +17,8 @@ class BalanceService:
 
     def _get_balance(self):
         return self._balance
+    
+    def _get_earnings(self):
+        return self._total_earnings
 
 balance_service = BalanceService()
--- src/ui/balance_view.py
+++ src/ui/balance_view.py
@@ -5,6 +5,7 @@ class BalanceView:
     def __init__(self, root):
         self._root = root
         self._balance_var = None
+        self._total_earnings_var = None
         self._frame = None
 
         self._initialize()
@@ -23,7 +24,8 @@ class BalanceView:
         self._layout_widgets()
 
     def _create_widgets(self):
-        self._expenses_view = ttk.Frame(master=self._frame)
+        self._total_earnings = ttk.Label(master=self._frame, text="total earnings", font=(20))
+        self._total_earnings_amount = ttk.Label(master=self._frame, textvariable=self._total_earnings_var)
         self._balance = ttk.Label(master=self._frame, text="balance", font=(20))
         self._balance_entry = ttk.Entry(master=self._frame)
         self._balance_amount = ttk.Label(master=self._frame, textvariable=self._balance_var)
@@ -32,12 +34,17 @@ class BalanceView:
 
     def _init_variables(self):
         balance = balance_service._get_balance()
+        total_earnings = balance_service._get_earnings()
         self._balance_var = StringVar()
+        self._total_earnings_var = StringVar()
         self._balance_var.set(balance)
+        self._total_earnings_var.set(total_earnings)
 
     def _layout_widgets(self):
-        self._balance.grid(row=0, column=0, sticky=constants.W, padx=10)
-        self._balance_amount.grid(row=0, column=1, sticky=constants.W, padx=10)
+        self._total_earnings.grid(row=0, column=0, sticky=constants.W, padx=10)
+        self._total_earnings_amount.grid(row=0, column=1, sticky=constants.W, padx=10)
+        self._balance.grid(row=1, column=0, sticky=constants.W, padx=10)
+        self._balance_amount.grid(row=1, column=1, sticky=constants.W, padx=10)
         self._balance_entry.grid(row=2, column=0)
         self._increase_button.grid(row=2, column=1)
         self._decrease_button.grid(row=2, column=2)
@@ -45,7 +52,8 @@ class BalanceView:
     def _increase_balance(self):
         increment = self._balance_entry.get()
         new_balance = balance_service.increase_balance(increment)
-        self._balance_var.set(str(new_balance))
+        self._balance_var.set(str(new_balance[0]))
+        self._total_earnings_var.set(str(new_balance[1]))
 
     def _decrease_balance(self, subtraction=None):
         decrement = int(self._balance_entry.get())
