84e4677a670f56d1217345ea07608fc4a68beedd
Olavi Suojala <o.suojala@gmail.com>
2025-12-09 19:33:34 +0200

    Fix balance service tests

--- src/tests/balance_service_test.py
+++ src/tests/balance_service_test.py
@@ -1,13 +1,52 @@
+import sqlite3
 import unittest
+from configparser import ParsingError
+
 from services.balance_service import BalanceService
+
+DB_FILE = "src/budget.db"
+
+
+def get_connection():
+    conn = sqlite3.connect(DB_FILE)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    return conn
+
+
+test_user_id = 222
+test_username = "pasi"
+test_password = "kuikka"
+
 
 class TestBalanceService(unittest.TestCase):
     def setUp(self):
-        self.balance = BalanceService(0)
+        self.conn = get_connection()
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("DELETE FROM balances WHERE user_id = ?", (test_user_id,))
+        self.cursor.execute("DELETE FROM users WHERE id = ?", (test_user_id,))
+        self.cursor.execute("DELETE FROM expenses WHERE user_id = ?", (test_user_id,))
+
+        try:
+            self.cursor.execute(
+                "INSERT INTO users (id, username, password) VALUES (?, ?, ?)",
+                (test_user_id, test_username, test_password),
+            )
+            self.cursor.execute(
+                "INSERT INTO balances (user_id, balance) VALUES (?, ?)",
+                (test_user_id, 0),
+            )
+            print("User added")
+        except:
+            print("Error in test setup")
+
+        self.conn.commit()
+        self.conn.close()
+
+        self.balance = BalanceService()
 
     def test_balance_is_zero_at_start(self):
-        self.assertEqual(self.balance._get_balance(), "0")
+        self.assertEqual(self.balance._get_balance(test_user_id), 0)
 
     def test_balance_is_increased_right(self):
-        self.balance.increase_balance(1000)
-        self.assertEqual(self.balance._get_balance(), "1000")
+        self.balance.increase_balance(1000, test_user_id)
+        self.assertEqual(self.balance._get_balance(test_user_id), 1000)
